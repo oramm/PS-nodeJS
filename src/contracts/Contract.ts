2121c4a1b2dfe73107018d2e9e7e5afb2beeb416
oramm <marek@envi.com.pl>
2021-09-13 07:35:54 +0200

    usunięcie ContracUrl

--- src/contracts/Contract.ts
+++ src/contracts/Contract.ts
@@ -2,116 +2,114 @@ import ToolsGd from '../tools/ToolsGd';
 import ToolsDate from '../tools/ToolsDate';
 
 export default class Contract {
-  id?: number;
-  alias?: string;
-  typeId?: number;
-  _type?: any;
-  _tmpId?: any;
-  number?: string;
-  name?: string;
-  _ourContract?: any;
-  ourIdRelated?: string;
-  projectId?: string;
-  startDate?: string;
-  endDate?: string;
-  value?: any;
-  comment?: string;
-  ourId?: string;
-  _ourType?: any;
-  gdFolderId?: string;
-  _gdFolderUrl?: string;
-  meetingProtocolsGdFolderId?: string;
-  materialCardsGdFolderId?: string;
-  _ourIdName?: string;
-  _numberName?: string;
-  _ourIdOrNumber_Name?: string;
-  _ourIdOrNumber_Alias?: string;
-  _manager?: any;
-  _admin?: any;
-  _contractors?: any[];
-  _engineers?: any[];
-  _employers?: any[];
-  contractUrl?: string;
-  gdUrl?: string;
-  status?: string;
-  scrumSheet?: any;
-  _parent?: any;
+    id?: number;
+    alias?: string;
+    typeId?: number;
+    _type?: any;
+    _tmpId?: any;
+    number?: string;
+    name?: string;
+    _ourContract?: any;
+    ourIdRelated?: string;
+    projectId?: string;
+    startDate?: string;
+    endDate?: string;
+    value?: any;
+    comment?: string;
+    ourId?: string;
+    _ourType?: any;
+    gdFolderId?: string;
+    _gdFolderUrl?: string;
+    meetingProtocolsGdFolderId?: string;
+    materialCardsGdFolderId?: string;
+    _ourIdName?: string;
+    _numberName?: string;
+    _ourIdOrNumber_Name?: string;
+    _ourIdOrNumber_Alias?: string;
+    _manager?: any;
+    _admin?: any;
+    _contractors?: any[];
+    _engineers?: any[];
+    _employers?: any[];
+    gdUrl?: string;
+    status?: string;
+    scrumSheet?: any;
+    _parent?: any;
 
-  constructor(initParamObject: any, conn?: GoogleAppsScript.JDBC.JdbcConnection) {
-    if (initParamObject) {
-      this.id = initParamObject.id;
-      this.alias = initParamObject.alias;
-      this.typeId = initParamObject._type.id;
-      this._type = initParamObject._type;
-      //id tworzone tymczasowo po stronie klienta do obsługi tymczasowego wiersza resultsecie
-      this._tmpId = initParamObject._tmpId;
-      this.number = initParamObject.number;
-      this.name = initParamObject.name;
-      //kontrakt na roboty może być obsługiwany przez ourContract
-      if (initParamObject._ourContract && initParamObject._ourContract.ourId) {
-        if (initParamObject.ourId) throw new Error("Nie można powiązać ze sobą dwóch Umów ENVI!!!");
+    constructor(initParamObject: any, conn?: GoogleAppsScript.JDBC.JdbcConnection) {
+        if (initParamObject) {
+            this.id = initParamObject.id;
+            this.alias = initParamObject.alias;
+            this.typeId = initParamObject._type.id;
+            this._type = initParamObject._type;
+            //id tworzone tymczasowo po stronie klienta do obsługi tymczasowego wiersza resultsecie
+            this._tmpId = initParamObject._tmpId;
+            this.number = initParamObject.number;
+            this.name = initParamObject.name;
+            //kontrakt na roboty może być obsługiwany przez ourContract
+            if (initParamObject._ourContract && initParamObject._ourContract.ourId) {
+                if (initParamObject.ourId) throw new Error("Nie można powiązać ze sobą dwóch Umów ENVI!!!");
 
-        this._ourContract = initParamObject._ourContract;
-        this._ourContract.ourId = initParamObject._ourContract.ourId.toUpperCase();
-        this._ourContract._ourType = this.getType(initParamObject._ourContract.ourId);
-        this._ourContract._gdFolderUrl = ToolsGd.createGdFolderUrl(initParamObject._ourContract.gdFolderId);
-        if (initParamObject._ourContract.name)
-          this._ourContract._ourIdName = initParamObject._ourContract.ourId + ' ' + initParamObject._ourContract.name.substr(0, 50) + '...';
-        this.ourIdRelated = initParamObject._ourContract.ourId;
-      }
-      if (initParamObject._ourContract) this.ourIdRelated = initParamObject._ourContract.ourId;
-      this.projectId = initParamObject.projectId;
-      this.startDate = ToolsDate.dateJsToSql(initParamObject.startDate);
-      this.endDate = ToolsDate.dateJsToSql(initParamObject.endDate);
+                this._ourContract = initParamObject._ourContract;
+                this._ourContract.ourId = initParamObject._ourContract.ourId.toUpperCase();
+                this._ourContract._ourType = this.getType(initParamObject._ourContract.ourId);
+                this._ourContract._gdFolderUrl = ToolsGd.createGdFolderUrl(initParamObject._ourContract.gdFolderId);
+                if (initParamObject._ourContract.name)
+                    this._ourContract._ourIdName = initParamObject._ourContract.ourId + ' ' + initParamObject._ourContract.name.substr(0, 50) + '...';
+                this.ourIdRelated = initParamObject._ourContract.ourId;
+            }
+            if (initParamObject._ourContract) this.ourIdRelated = initParamObject._ourContract.ourId;
+            this.projectId = initParamObject.projectId;
+            this.startDate = ToolsDate.dateJsToSql(initParamObject.startDate);
+            this.endDate = ToolsDate.dateJsToSql(initParamObject.endDate);
 
-      this.value = initParamObject.value;
-      this.comment = initParamObject.comment;
+            this.value = initParamObject.value;
+            this.comment = initParamObject.comment;
 
-      if (initParamObject.ourId) {
-        this.ourId = initParamObject.ourId.toUpperCase();
-        this._ourType = this.getType(this.ourId as string);
-      }
-      if (initParamObject.gdFolderId) {
-        this.gdFolderId = initParamObject.gdFolderId;
-        this._gdFolderUrl = 'https://drive.google.com/drive/folders/' + initParamObject.gdFolderId;
-      }
-      this.meetingProtocolsGdFolderId = initParamObject.meetingProtocolsGdFolderId;
-      this.materialCardsGdFolderId = initParamObject.materialCardsGdFolderId;
-      if (initParamObject.ourId && this.name)
-        this._ourIdName = initParamObject.ourId + ' ' + initParamObject.name.substr(0, 50) + '...';
-      else if (this.name)
-        this._numberName = initParamObject.number + ' ' + initParamObject.name.substr(0, 50) + '...';
+            if (initParamObject.ourId) {
+                this.ourId = initParamObject.ourId.toUpperCase();
+                this._ourType = this.getType(this.ourId as string);
+            }
+            if (initParamObject.gdFolderId) {
+                this.gdFolderId = initParamObject.gdFolderId;
+                this._gdFolderUrl = 'https://drive.google.com/drive/folders/' + initParamObject.gdFolderId;
+            }
+            this.meetingProtocolsGdFolderId = initParamObject.meetingProtocolsGdFolderId;
+            this.materialCardsGdFolderId = initParamObject.materialCardsGdFolderId;
+            if (initParamObject.ourId && this.name)
+                this._ourIdName = initParamObject.ourId + ' ' + initParamObject.name.substr(0, 50) + '...';
+            else if (this.name)
+                this._numberName = initParamObject.number + ' ' + initParamObject.name.substr(0, 50) + '...';
 
-      //znacznik uniwersalny gdy chemy wybierać ze wszystkich kontraktów Our i Works
-      var _ourIdOrNumber = '';
-      if (this.ourId) _ourIdOrNumber = this.ourId;
-      else if (this.number) _ourIdOrNumber = this.number;
+            //znacznik uniwersalny gdy chemy wybierać ze wszystkich kontraktów Our i Works
+            var _ourIdOrNumber = '';
+            if (this.ourId) _ourIdOrNumber = this.ourId;
+            else if (this.number) _ourIdOrNumber = this.number;
 
-      if (this.name) {
-        this._ourIdOrNumber_Name = _ourIdOrNumber + ' ' + this.name.substr(0, 50) + '...'
-      }
-      this._ourIdOrNumber_Alias = _ourIdOrNumber;
-      if (this.alias)
-        this._ourIdOrNumber_Alias += ' ' + this.alias;
+            if (this.name) {
+                this._ourIdOrNumber_Name = _ourIdOrNumber + ' ' + this.name.substr(0, 50) + '...'
+            }
+            this._ourIdOrNumber_Alias = _ourIdOrNumber;
+            if (this.alias)
+                this._ourIdOrNumber_Alias += ' ' + this.alias;
 
-      this._manager = (initParamObject._manager) ? initParamObject._manager : {};
-      this._admin = (initParamObject._admin) ? initParamObject._admin : {};
+            this._manager = (initParamObject._manager) ? initParamObject._manager : {};
+            this._admin = (initParamObject._admin) ? initParamObject._admin : {};
 
-      this._contractors = (initParamObject._contractors) ? initParamObject._contractors : [];
+            this._contractors = (initParamObject._contractors) ? initParamObject._contractors : [];
 
-      this._engineers = (initParamObject._engineers) ? initParamObject._engineers : [];
-      this._employers = (initParamObject._employers) ? initParamObject._employers : [];
+            this._engineers = (initParamObject._engineers) ? initParamObject._engineers : [];
+            this._employers = (initParamObject._employers) ? initParamObject._employers : [];
 
-      this.contractUrl = initParamObject.contractUrl;
-      this.gdUrl = initParamObject.gdUrl;
+            this.gdUrl = initParamObject.gdUrl;
 
-      this.status = initParamObject.status;
+            this.status = initParamObject.status;
 
-      //this.scrumSheet = new ScrumSheet();
+            //this.scrumSheet = new ScrumSheet();
+        }
     }
-  }
 
-  getType(ourId: string): string {
-    return ourId.substring(ourId.indexOf('.') + 1, ourId.lastIndexOf('.'));
-  }
+    getType(ourId: string): string {
+        return ourId.substring(ourId.indexOf('.') + 1, ourId.lastIndexOf('.'));
+    }
 }
--- src/contracts/ContractsController.ts
+++ src/contracts/ContractsController.ts
@@ -6,15 +6,15 @@ import ToolsDb from '../tools/ToolsDb'
 import Contract from "./Contract";
 
 export default class ContractsController {
-  static async getContractsList(initParamObject: any) {
-    const projectCondition = (initParamObject && initParamObject.projectId) ? 'mainContracts.ProjectOurId="' + initParamObject.projectId + '"' : '1';
-    const contractCondition = (initParamObject && initParamObject.contractId) ? 'mainContracts.Id=' + initParamObject.contractId : '1';
-    const contractOurIdCondition = (initParamObject && initParamObject.contractOurId) ? 'OurContractsData.OurId="' + initParamObject.contractOurId + '"' : '1';
+    static async getContractsList(initParamObject: any) {
+        const projectCondition = (initParamObject && initParamObject.projectId) ? 'mainContracts.ProjectOurId="' + initParamObject.projectId + '"' : '1';
+        const contractCondition = (initParamObject && initParamObject.contractId) ? 'mainContracts.Id=' + initParamObject.contractId : '1';
+        const contractOurIdCondition = (initParamObject && initParamObject.contractOurId) ? 'OurContractsData.OurId="' + initParamObject.contractOurId + '"' : '1';
 
-    const onlyOurContractsCondition = (initParamObject && initParamObject.onlyOur) ? 'OurContractsData.OurId IS NOT NULL' : '1';
-    const isArchivedConditon = (initParamObject && initParamObject.isArchived) ? 'mainContracts.Status="Archiwalny"' : 'mainContracts.Status!="Archiwalny"';
+        const onlyOurContractsCondition = (initParamObject && initParamObject.onlyOur) ? 'OurContractsData.OurId IS NOT NULL' : '1';
+        const isArchivedConditon = (initParamObject && initParamObject.isArchived) ? 'mainContracts.Status="Archiwalny"' : 'mainContracts.Status!="Archiwalny"';
 
-    const sql = `SELECT mainContracts.Id, 
+        const sql = `SELECT mainContracts.Id, 
         mainContracts.Alias, 
         mainContracts.Number, 
         mainContracts.Name, 
@@ -54,177 +54,175 @@ export default class ContractsController {
       WHERE ${projectCondition} AND ${contractCondition} AND ${onlyOurContractsCondition} AND ${contractOurIdCondition} AND ${isArchivedConditon}
       ORDER BY OurContractsData.OurId DESC, mainContracts.Number`;
 
-    const result: any[] = <any[]>await ToolsDb.getQueryCallbackAsync(sql);
-    return (initParamObject.onlyKeyData) ? this.processContractsResultKeyData(result, initParamObject) : this.processContractsResult(result, initParamObject);
-  }
-
-  private static async processContractsResult(result: any[], initParamObject: any) {
-    let newResult: [Contract?] = [];
-    let entitiesPerProject: any[] = [];
-
-    if (initParamObject.projectId)
-      entitiesPerProject = await this.getContractEntityAssociationsList(initParamObject);
-    for (const row of result) {
-
-      var contractors = entitiesPerProject.filter((item: any) =>
-        item._contract.id == row.Id && item.contractRole == 'CONTRACTOR'
-      );
-      var engineers = entitiesPerProject.filter((item: any) =>
-        item._contract.id == row.Id && item.contractRole == 'ENGINEER'
-      );
-      var employers = entitiesPerProject.filter((item: any) =>
-        item._contract.id == row.Id && item.contractRole == 'EMPLOYER'
-      );
-      const item = new Contract({
-        id: row.Id,
-        alias: row.Alias,
-        number: row.Number,
-        name: ToolsDb.sqlToString(row.Name),
-        //kontrakt powiązany z kontraktem na roboty
-        _ourContract: {
-          ourId: row.OurIdRelated,
-          id: row.RelatedId,
-          name: ToolsDb.sqlToString(row.RelatedName),
-          gdFolderId: row.RelatedGdFolderId
-        },
-        projectId: row.ProjectOurId,
-        startDate: row.StartDate,
-        endDate: row.EndDate,
-        value: row.Value,
-        comment: ToolsDb.sqlToString(row.Comment),
-        status: row.Status,
-        gdFolderId: row.GdFolderId,
-        meetingProtocolsGdFolderId: row.MeetingProtocolsGdFolderId,
-        materialCardsGdFolderId: row.MaterialCardsGdFolderId,
-        ourId: row.OurId,
-        _manager: new Person({
-          id: row.ManagerId,
-          name: row.ManagerName,
-          surname: row.ManagerSurname,
-          email: row.ManagerEmail
-        }),
-        _admin: new Person({
-          id: row.AdminId,
-          name: row.AdminName,
-          surname: row.AdminSurname,
-          email: row.AdminEmail
-        }),
-        contractUrl: row.ContractURL,
-        _type: {
-          id: row.TypeId,
-          name: row.TypeName,
-          description: row.TypeDescription,
-          isOur: row.TypeIsOur
-        },
-        _contractors: contractors.map((item: any) => item._entity),
-        _engineers: engineers.map((item: any) => item._entity),
-        _employers: employers.map((item: any) => item._entity)
-      });
-      newResult.push(item);
+        const result: any[] = <any[]>await ToolsDb.getQueryCallbackAsync(sql);
+        return (initParamObject.onlyKeyData) ? this.processContractsResultKeyData(result, initParamObject) : this.processContractsResult(result, initParamObject);
     }
-    return newResult;
-  }
 
-  private static processContractsResultKeyData(result: any[], initParamObject: any): [Contract?] {
-    let newResult: [Contract?] = [];
+    private static async processContractsResult(result: any[], initParamObject: any) {
+        let newResult: [Contract?] = [];
+        let entitiesPerProject: any[] = [];
 
-    for (const row of result) {
-      const item = new Contract({
-        id: row.Id,
-        alias: row.Alias,
-        number: row.Number,
-        name: ToolsDb.sqlToString(row.Name),
-        //kontrakt powiązany z kontraktem na roboty
-        _ourContract: {
-          ourId: row.OurIdRelated,
-          id: row.RelatedId,
-          name: ToolsDb.sqlToString(row.RelatedName),
-          gdFolderId: row.RelatedGdFolderId
-        },
-        projectId: row.ProjectOurId,
-        startDate: row.StartDate,
-        endDate: row.EndDate,
-        value: row.Value,
-        comment: ToolsDb.sqlToString(row.Comment),
-        status: row.Status,
-        gdFolderId: row.GdFolderId,
-        meetingProtocolsGdFolderId: row.MeetingProtocolsGdFolderId,
-        materialCardsGdFolderId: row.MaterialCardsGdFolderId,
-        ourId: row.OurId,
-        _manager: {
-          id: row.ManagerId,
-          name: row.ManagerName,
-          surname: row.ManagerSurname,
-          email: row.ManagerEmail
-        },
-        _admin: {
-          id: row.AdminId,
-          name: row.AdminName,
-          surname: row.AdminSurname,
-          email: row.AdminEmail
-        },
-        contractUrl: row.ContractURL,
-        _type: {
-          id: row.TypeId,
-          name: row.TypeName,
-          description: row.TypeDescription,
-          isOur: row.TypeIsOur
-        },
-      });
+        if (initParamObject.projectId)
+            entitiesPerProject = await this.getContractEntityAssociationsList(initParamObject);
+        for (const row of result) {
 
-      newResult.push(item);
+            var contractors = entitiesPerProject.filter((item: any) =>
+                item._contract.id == row.Id && item.contractRole == 'CONTRACTOR'
+            );
+            var engineers = entitiesPerProject.filter((item: any) =>
+                item._contract.id == row.Id && item.contractRole == 'ENGINEER'
+            );
+            var employers = entitiesPerProject.filter((item: any) =>
+                item._contract.id == row.Id && item.contractRole == 'EMPLOYER'
+            );
+            const item = new Contract({
+                id: row.Id,
+                alias: row.Alias,
+                number: row.Number,
+                name: ToolsDb.sqlToString(row.Name),
+                //kontrakt powiązany z kontraktem na roboty
+                _ourContract: {
+                    ourId: row.OurIdRelated,
+                    id: row.RelatedId,
+                    name: ToolsDb.sqlToString(row.RelatedName),
+                    gdFolderId: row.RelatedGdFolderId
+                },
+                projectId: row.ProjectOurId,
+                startDate: row.StartDate,
+                endDate: row.EndDate,
+                value: row.Value,
+                comment: ToolsDb.sqlToString(row.Comment),
+                status: row.Status,
+                gdFolderId: row.GdFolderId,
+                meetingProtocolsGdFolderId: row.MeetingProtocolsGdFolderId,
+                materialCardsGdFolderId: row.MaterialCardsGdFolderId,
+                ourId: row.OurId,
+                _manager: new Person({
+                    id: row.ManagerId,
+                    name: row.ManagerName,
+                    surname: row.ManagerSurname,
+                    email: row.ManagerEmail
+                }),
+                _admin: new Person({
+                    id: row.AdminId,
+                    name: row.AdminName,
+                    surname: row.AdminSurname,
+                    email: row.AdminEmail
+                }),
+                _type: {
+                    id: row.TypeId,
+                    name: row.TypeName,
+                    description: row.TypeDescription,
+                    isOur: row.TypeIsOur
+                },
+                _contractors: contractors.map((item: any) => item._entity),
+                _engineers: engineers.map((item: any) => item._entity),
+                _employers: employers.map((item: any) => item._entity)
+            });
+            newResult.push(item);
+        }
+        return newResult;
     }
-    return newResult;
-  }
 
-  static async getContractEntityAssociationsList(initParamObject: { projectId: string, contractId: string, isArchived: string }) {
-    let projectConditon = (initParamObject && initParamObject.projectId) ? 'Contracts.ProjectOurId="' + initParamObject.projectId + '"' : '1';
-    let contractConditon = (initParamObject && initParamObject.contractId) ? 'Contracts.Id="' + initParamObject.contractId + '"' : '1';
-    const sql = 'SELECT  Contracts_Entities.ContractId, \n \t' +
-      'Contracts_Entities.EntityId, \n \t' +
-      'Contracts_Entities.ContractRole, \n \t' +
-      'Entities.Name, \n \t' +
-      'Entities.Address, \n \t' +
-      'Entities.TaxNumber, \n \t' +
-      'Entities.Www, \n \t' +
-      'Entities.Email, \n \t' +
-      'Entities.Phone, \n \t' +
-      'Entities.Fax \n' +
-      'FROM Contracts_Entities \n' +
-      'JOIN Contracts ON Contracts_Entities.ContractId = Contracts.Id \n' +
-      'JOIN Entities ON Contracts_Entities.EntityId=Entities.Id \n' +
-      'LEFT JOIN OurContractsData ON OurContractsData.Id=Contracts.Id \n' +
-      'WHERE ' + projectConditon + ' AND ' + contractConditon + ' \n' +
-      'ORDER BY Contracts_Entities.ContractRole, Entities.Name';
-    const result: any[] = <any[]>await ToolsDb.getQueryCallbackAsync(sql);
+    private static processContractsResultKeyData(result: any[], initParamObject: any): [Contract?] {
+        let newResult: [Contract?] = [];
 
-    return this.processContractEntityAssociations(result);
-  }
+        for (const row of result) {
+            const item = new Contract({
+                id: row.Id,
+                alias: row.Alias,
+                number: row.Number,
+                name: ToolsDb.sqlToString(row.Name),
+                //kontrakt powiązany z kontraktem na roboty
+                _ourContract: {
+                    ourId: row.OurIdRelated,
+                    id: row.RelatedId,
+                    name: ToolsDb.sqlToString(row.RelatedName),
+                    gdFolderId: row.RelatedGdFolderId
+                },
+                projectId: row.ProjectOurId,
+                startDate: row.StartDate,
+                endDate: row.EndDate,
+                value: row.Value,
+                comment: ToolsDb.sqlToString(row.Comment),
+                status: row.Status,
+                gdFolderId: row.GdFolderId,
+                meetingProtocolsGdFolderId: row.MeetingProtocolsGdFolderId,
+                materialCardsGdFolderId: row.MaterialCardsGdFolderId,
+                ourId: row.OurId,
+                _manager: {
+                    id: row.ManagerId,
+                    name: row.ManagerName,
+                    surname: row.ManagerSurname,
+                    email: row.ManagerEmail
+                },
+                _admin: {
+                    id: row.AdminId,
+                    name: row.AdminName,
+                    surname: row.AdminSurname,
+                    email: row.AdminEmail
+                },
+                _type: {
+                    id: row.TypeId,
+                    name: row.TypeName,
+                    description: row.TypeDescription,
+                    isOur: row.TypeIsOur
+                },
+            });
 
-  private static processContractEntityAssociations(result: any[]): [any?] {
-    let newResult: [any?] = [];
-
-    for (const row of result) {
-      const item = {
-        contractRole: row.ContractRole,
-        _contract: {
-          id: row.ContractId
-        },
-        _entity: new Entity({
-          id: row.EntityId,
-          name: row.Name,
-          address: row.Address,
-          taxNumber: row.TaxNumber,
-          www: row.Www,
-          email: row.Email,
-          phone: row.Phone,
-          fax: row.Fax
-        })
-      };
-
-      newResult.push(item);
+            newResult.push(item);
+        }
+        return newResult;
     }
-    return newResult;
 
-  }
+    static async getContractEntityAssociationsList(initParamObject: { projectId: string, contractId: string, isArchived: string }) {
+        let projectConditon = (initParamObject && initParamObject.projectId) ? 'Contracts.ProjectOurId="' + initParamObject.projectId + '"' : '1';
+        let contractConditon = (initParamObject && initParamObject.contractId) ? 'Contracts.Id="' + initParamObject.contractId + '"' : '1';
+        const sql = 'SELECT  Contracts_Entities.ContractId, \n \t' +
+            'Contracts_Entities.EntityId, \n \t' +
+            'Contracts_Entities.ContractRole, \n \t' +
+            'Entities.Name, \n \t' +
+            'Entities.Address, \n \t' +
+            'Entities.TaxNumber, \n \t' +
+            'Entities.Www, \n \t' +
+            'Entities.Email, \n \t' +
+            'Entities.Phone, \n \t' +
+            'Entities.Fax \n' +
+            'FROM Contracts_Entities \n' +
+            'JOIN Contracts ON Contracts_Entities.ContractId = Contracts.Id \n' +
+            'JOIN Entities ON Contracts_Entities.EntityId=Entities.Id \n' +
+            'LEFT JOIN OurContractsData ON OurContractsData.Id=Contracts.Id \n' +
+            'WHERE ' + projectConditon + ' AND ' + contractConditon + ' \n' +
+            'ORDER BY Contracts_Entities.ContractRole, Entities.Name';
+        const result: any[] = <any[]>await ToolsDb.getQueryCallbackAsync(sql);
+
+        return this.processContractEntityAssociations(result);
+    }
+
+    private static processContractEntityAssociations(result: any[]): [any?] {
+        let newResult: [any?] = [];
+
+        for (const row of result) {
+            const item = {
+                contractRole: row.ContractRole,
+                _contract: {
+                    id: row.ContractId
+                },
+                _entity: new Entity({
+                    id: row.EntityId,
+                    name: row.Name,
+                    address: row.Address,
+                    taxNumber: row.TaxNumber,
+                    www: row.Www,
+                    email: row.Email,
+                    phone: row.Phone,
+                    fax: row.Fax
+                })
+            };
+
+            newResult.push(item);
+        }
+        return newResult;
+
+    }
 }
