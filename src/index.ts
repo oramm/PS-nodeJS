928874e8d99c1760de16118ea2bbd1dc8bae0b01
Marek Gazda <33371875+oramm@users.noreply.github.com>
2024-03-18 14:07:18 +0100

    porawka x

--- src/index.ts
+++ src/index.ts
@@ -189,7 +189,7 @@ require('./processes/processInstances/ProcessStepInstancesRouters');
 require('./documentTemplates/DocumentTemplatesRouters');
 
 require('./letters/LettersRouters');
-require('./offers1/OffersRouters');
+require('./offers/OffersRouters');
 
 const meetingsRouter = require('./meetings/MeetingsRouters');
 app.use(meetingsRouter);
