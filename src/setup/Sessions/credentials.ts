fb4ea49962857028084861b60f3649cf86885cbd
MicKota <kotalamichal02@gmail.com>
2025-09-09 10:44:21 +0200

    credentials

--- src/setup/Sessions/credentials.ts
+++ src/setup/Sessions/credentials.ts
@@ -1,7 +1,7 @@
 export const keys = {
     "installed": {
-        "client_id": "246174537725-7t658k3s4u5fsi35jjs4si7ukqlnaujb.apps.googleusercontent.com",
-        "project_id": "erp-envi-1611690452900",
+        "client_id": process.env.GOOGLE_CLIENT_ID as string,
+        "project_id": process.env.GOOGLE_PROJECT_ID as string,
         "auth_uri": "https://accounts.google.com/o/oauth2/auth",
         "redirect_uris": [
             "http://localhost/Moje.ENVI/docs/",
@@ -9,7 +9,7 @@ export const keys = {
         ],
         "token_uri": "https://oauth2.googleapis.com/token",
         "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
-        "client_secret": "a-4Yh4X8lv4t3Yw7xjKjILsL",
+        "client_secret": process.env.GOOGLE_CLIENT_SECRET as string,
         "javascript_origins": [
             "http://localhost",
             "https://oramm.github.io",
@@ -21,11 +21,11 @@ export const keys = {
         ]
     },
     "mongoDb": {
-        "host": "localhost",
-        "port": "27017",
-        "database": "envi",
-        "user": "envi",
-        "password": "mYnChcuBGRUTCU1X",
-        "uri": 'mongodb+srv://envi:mYnChcuBGRUTCU1X@cluster0.yca9wrm.mongodb.net/?retryWrites=true&w=majority'
+        host: process.env.MONGO_HOST as string,
+        port: process.env.MONGO_PORT as string,
+        database: process.env.MONGO_DATABASE as string,
+        user: process.env.MONGO_USER as string,
+        password: process.env.MONGO_PASSWORD as string,
+        uri: process.env.MONGO_URI as string,
     }
 }
